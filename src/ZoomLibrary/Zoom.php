4926949a620400fb38a6ba758644c1af3fb10cbc
aghilanbaskar <aghilanbaskar@gmail.com>
2021-05-23 21:38:16 +0530

    create and delete - meeting added

--- src/ZoomLibrary/Zoom.php
+++ src/ZoomLibrary/Zoom.php
@@ -94,12 +94,112 @@ class Zoom
         return array('status' => true, 'data' => json_decode($response->getBody(), true));
       } catch (\Exception $e) {
         if( $e->getCode() == 401 && $this->refreshToken()) {
-          $this->listMeeting($user_id, $query);
+          return $this->listMeeting($user_id, $query);
         } else {
           return array('status' => false, 'message' => $e->getMessage());
         }
       }
     }
+
+  public function createMeeting($user_id = 'me',$json = [])
+  {
+    try {
+      $response = $this->CLIENT->request('POST', "/v2/users/{$user_id}/meetings", [
+          "headers" => [
+              "Authorization" => "Bearer ".$this->CREDENTIAL_DATA['access_token']
+          ],
+          'json' => $json
+      ]);
+
+      if ($response->getStatusCode() == 201) {
+        return array('status' => true, 'data' => json_decode($response->getBody(), true));
+      }
+
+      throw new Exception("Not able to find error");
+    } catch (\Exception $e) {
+      if( $e->getCode() == 401 && $this->refreshToken()) {
+        return $this->createMeeting($user_id, $json);
+      }
+      if ($e->getCode() == 300) {
+        return array('status' => false, 'message' => 'Invalid enforce_login_domains, separate multiple domains by semicolon. A maximum of {rateLimitNumber} meetings can be created/updated for a single user in one day.');
+      }
+      if ($e->getCode() == 404) {
+        return array('status' => false, 'message' => 'User {userId} not exist or not belong to this account.');
+      }
+      if( $e->getCode() != 401 ) {
+        return array('status' => false, 'message' => $e->getMessage());
+      }
+      return array('status' => false, 'message' => 'Not able to refresh token');
+    }
+  }
+
+  public function deleteMeeting($meeting_id = '', $query = [])
+  {
+    try {
+      $response = $this->CLIENT->request('DELETE', "/v2/meetings/{$meeting_id}", [
+          "headers" => [
+              "Authorization" => "Bearer ".$this->CREDENTIAL_DATA['access_token']
+          ],
+          'query' => $query
+      ]);
+
+      if ($response->getStatusCode() == 204) {
+        return array('status' => true, 'message' => 'Meeting deleted.');
+      }
+      throw new Exception("Not able to find error");
+    } catch (\Exception $e) {
+      if( $e->getCode() == 401 && $this->refreshToken()) {
+        return $this->deleteMeeting($meeting_id, $query);
+      }
+      if ($e->getCode() == 400) {
+        return array('status' => false, 'message' => 'User does not belong to this account or dont have access');
+      }
+      if ($e->getCode() == 404) {
+        return array('status' => false, 'message' => 'Meeting with this {meetingId} is not found or has expired.');
+      }
+      if( $e->getCode() != 401 ) {
+        return array('status' => false, 'message' => $e->getMessage());
+      }
+      return array('status' => false, 'message' => 'Not able to refresh token');
+    }
+  }
+
+
+  public function addMeetingRegistrant($meeting_id = '', $json = [])
+  {
+    try {
+      $response = $this->CLIENT->request('POST', "/v2/meetings/{$meeting_id}/registrants", [
+          "headers" => [
+              "Authorization" => "Bearer ".$this->CREDENTIAL_DATA['access_token']
+          ],
+          'json' => $json
+      ]);
+
+      if ($response->getStatusCode() == 201) {
+        return array('status' => true, 'message' => 'Registration successfull', 'data' => json_decode($response->getBody(), true) );
+      }
+
+      throw new Exception("Not able to find error");
+    }
+    catch (\Exception $e) {
+      if( $e->getCode() == 401 && $this->refreshToken() ) {
+        return $this->addMeetingRegistrant($meeting_id, $json);
+      }
+      if ($e->getCode() == 300) {
+        return array('status' => false, 'message' => 'Meeting {meetingId} is not found or has expired.');
+      }
+      if ($e->getCode() == 400) {
+        return array('status' => false, 'message' => 'Access error. Not have correct access. validation failed');
+      }
+      if ($e->getCode() == 404) {
+        return array('status' => false, 'message' => 'Meeting not found or Meeting host does not exist: {userId}.');
+      }
+      if( $e->getCode() != 401 ) {
+        return array('status' => false, 'message' => $e->getMessage());
+      }
+      return array('status' => false, 'message' => 'Not able to refresh token');
+    }
+  }
 }
 
 ?>
